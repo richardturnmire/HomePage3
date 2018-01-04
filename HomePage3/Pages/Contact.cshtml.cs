8aca615839b410bf06e0c4707d412c043105e84e
Richard Turnmire <rturnmire@hotmail.com>
2018-01-04 02:52:34 -0500

    Format changes. Setting up Contact page.

--- HomePage3/Pages/Contact.cshtml.cs
+++ HomePage3/Pages/Contact.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,8 +9,25 @@ namespace HomePage3.Pages
 {
     public class ContactModel : PageModel
     {
+        [DataType(DataType.MultilineText)]
+        [Required]
         public string Message { get; set; }
+        
+        [DataType(DataType.Text)]
+        [Required]
+        public string Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
+        public string Subject { get; set; }
+        
         public void OnGet()
         {
             Message = "Your contact page.";
